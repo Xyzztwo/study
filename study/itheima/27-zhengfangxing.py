42a44187690f6b6bfefc1addbc896bf55386bc4e
Xyzztwo <xubaibai922@gmail.com>
2024-11-08 22:31:00 +0800

    3

---
+++ study/itheima/27-zhengfangxing.py
@@ -0,0 +1,33 @@
+i = 0
+while i < 5:
+    print("* ", end="")
+    j = 0
+    # while j < i 正三角
+    while j < 4:
+        print("* ", end="")
+        j += 1
+    print("")
+    i += 1
+
+# i = 0
+# while i < 5:
+#     j = 0
+#     while j < 5:
+#         print("*", end="")
+#         j += 1
+#     # /n
+#     print(" ")
+#     i += 1
+
+
+# j = 0
+# while j < 5:
+#     print("*" * 5, end="")
+#     print("")
+#     j += 1
+
+
+# a = 0
+# while a < 5:
+#     print("* " * 5)
+#     a += 1
